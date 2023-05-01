27076e832f56dbadf38d6f019e57c98c5f333378
MooMooHorse <haor.20@intl.zju.edu.cn>
2023-04-30 21:33:28 -0500

    add platform designer

--- lab7soc/synthesis/submodules/lab7soc_mm_interconnect_0_router_011.sv
+++ lab7soc/synthesis/submodules/lab7soc_mm_interconnect_0_router_011.sv
@@ -42,21 +42,21 @@
 
 `timescale 1 ns / 1 ns
 
-module lab7soc_mm_interconnect_0_router_002_default_decode
+module lab7soc_mm_interconnect_0_router_011_default_decode
   #(
      parameter DEFAULT_CHANNEL = 0,
                DEFAULT_WR_CHANNEL = -1,
                DEFAULT_RD_CHANNEL = -1,
                DEFAULT_DESTID = 0 
    )
-  (output [94 - 90 : 0] default_destination_id,
+  (output [76 - 72 : 0] default_destination_id,
    output [19-1 : 0] default_wr_channel,
    output [19-1 : 0] default_rd_channel,
    output [19-1 : 0] default_src_channel
   );
 
   assign default_destination_id = 
-    DEFAULT_DESTID[94 - 90 : 0];
+    DEFAULT_DESTID[76 - 72 : 0];
 
   generate
     if (DEFAULT_CHANNEL == -1) begin : no_default_channel_assignment
@@ -81,7 +81,7 @@ module lab7soc_mm_interconnect_0_router_002_default_decode
 endmodule
 
 
-module lab7soc_mm_interconnect_0_router_002
+module lab7soc_mm_interconnect_0_router_011
 (
     // -------------------
     // Clock & Reset
@@ -93,7 +93,7 @@ module lab7soc_mm_interconnect_0_router_002
     // Command Sink (Input)
     // -------------------
     input                       sink_valid,
-    input  [108-1 : 0]    sink_data,
+    input  [90-1 : 0]    sink_data,
     input                       sink_startofpacket,
     input                       sink_endofpacket,
     output                      sink_ready,
@@ -102,7 +102,7 @@ module lab7soc_mm_interconnect_0_router_002
     // Command Source (Output)
     // -------------------
     output                          src_valid,
-    output reg [108-1    : 0] src_data,
+    output reg [90-1    : 0] src_data,
     output reg [19-1 : 0] src_channel,
     output                          src_startofpacket,
     output                          src_endofpacket,
@@ -112,18 +112,18 @@ module lab7soc_mm_interconnect_0_router_002
     // -------------------------------------------------------
     // Local parameters and variables
     // -------------------------------------------------------
-    localparam PKT_ADDR_H = 63;
-    localparam PKT_ADDR_L = 36;
-    localparam PKT_DEST_ID_H = 94;
-    localparam PKT_DEST_ID_L = 90;
-    localparam PKT_PROTECTION_H = 98;
-    localparam PKT_PROTECTION_L = 96;
-    localparam ST_DATA_W = 108;
+    localparam PKT_ADDR_H = 45;
+    localparam PKT_ADDR_L = 18;
+    localparam PKT_DEST_ID_H = 76;
+    localparam PKT_DEST_ID_L = 72;
+    localparam PKT_PROTECTION_H = 80;
+    localparam PKT_PROTECTION_L = 78;
+    localparam ST_DATA_W = 90;
     localparam ST_CHANNEL_W = 19;
     localparam DECODER_TYPE = 1;
 
-    localparam PKT_TRANS_WRITE = 66;
-    localparam PKT_TRANS_READ  = 67;
+    localparam PKT_TRANS_WRITE = 48;
+    localparam PKT_TRANS_READ  = 49;
 
     localparam PKT_ADDR_W = PKT_ADDR_H-PKT_ADDR_L + 1;
     localparam PKT_DEST_ID_W = PKT_DEST_ID_H-PKT_DEST_ID_L + 1;
@@ -163,9 +163,14 @@ module lab7soc_mm_interconnect_0_router_002
 
 
 
+    // -------------------------------------------------------
+    // Write and read transaction signals
+    // -------------------------------------------------------
+    wire read_transaction;
+    assign read_transaction  = sink_data[PKT_TRANS_READ];
 
 
-    lab7soc_mm_interconnect_0_router_002_default_decode the_default_decode(
+    lab7soc_mm_interconnect_0_router_011_default_decode the_default_decode(
       .default_destination_id (),
       .default_wr_channel   (),
       .default_rd_channel   (),
@@ -185,7 +190,11 @@ module lab7soc_mm_interconnect_0_router_002
 
 
         if (destid == 0 ) begin
-            src_channel = 19'b1;
+            src_channel = 19'b01;
+        end
+
+        if (destid == 1  && read_transaction) begin
+            src_channel = 19'b10;
         end
 
 
